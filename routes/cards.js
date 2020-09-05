6f3d8562afe21abea5da99d4558f44a7a6de8e7b
Alexander Moiseyev <daklaktakpak@yandex.ru>
2020-09-05 18:14:53 +0500

    fix review remarks

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,7 @@
 const cardsRouter = require('express')
   .Router();
 const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 const {
   createCard,
   getCards,
@@ -10,31 +11,56 @@ const {
 } = require('../controllers/cards');
 const auth = require('../middlewares/auth');
 
+const method = (value, helpers) => {
+  if (!validator.isURL(value)) {
+    return helpers.error('any.invalid');
+  }
+  return value;
+};
+
 cardsRouter.get('/cards', auth, getCards);
 
 cardsRouter.post('/cards', auth, celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().uri(),
-  }),
+  body: Joi.object()
+    .keys({
+      name: Joi.string()
+        .required()
+        .min(2)
+        .max(30),
+      link: Joi.string()
+        .required()
+        .custom(method, 'custom validation'),
+    }),
 }), createCard);
 
 cardsRouter.delete('/cards/:cardId', auth, celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().required().length(24),
-  }),
+  params: Joi.object()
+    .keys({
+      cardId: Joi.string()
+        .required()
+        .hex()
+        .length(24),
+    }),
 }), deleteCard);
 
 cardsRouter.put('/cards/:cardId/likes', auth, celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().required().length(24),
-  }),
+  params: Joi.object()
+    .keys({
+      cardId: Joi.string()
+        .required()
+        .hex()
+        .length(24),
+    }),
 }), likeCard);
 
 cardsRouter.delete('/cards/:cardId/likes', auth, celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().required().length(24),
-  }),
+  params: Joi.object()
+    .keys({
+      cardId: Joi.string()
+        .required()
+        .hex()
+        .length(24),
+    }),
 }), dislikeCard);
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const usersRouter = require('express')
   .Router();
 const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 const {
   getUsers,
   createUser,
@@ -12,42 +13,81 @@ const {
 
 const auth = require('../middlewares/auth');
 
+const method = (value, helpers) => {
+  if (!validator.isURL(value)) {
+    return helpers.error('any.invalid');
+  }
+  return value;
+};
+
 usersRouter.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().uri(),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
+  body: Joi.object()
+    .keys({
+      name: Joi.string()
+        .required()
+        .min(2)
+        .max(30),
+      about: Joi.string()
+        .required()
+        .min(2)
+        .max(30),
+      avatar: Joi.string()
+        .required()
+        .custom(method, 'custom validation'),
+      email: Joi.string()
+        .required()
+        .email(),
+      password: Joi.string()
+        .required()
+        .min(8),
+    }),
 }), createUser);
 
 usersRouter.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
+  body: Joi.object()
+    .keys({
+      email: Joi.string()
+        .required()
+        .email(),
+      password: Joi.string()
+        .required()
+        .min(8),
+    }),
 }), login);
 
 usersRouter.get('/users', auth, getUsers);
 
 usersRouter.get('/users/:id', auth, celebrate({
-  params: Joi.object().keys({
-    id: Joi.string().required().length(24),
-  }),
+  params: Joi.object()
+    .keys({
+      id: Joi.string()
+        .required()
+        .hex()
+        .length(24),
+    }),
 }), findUser);
 
 usersRouter.patch('/users/me', auth, celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-  }),
+  body: Joi.object()
+    .keys({
+      name: Joi.string()
+        .required()
+        .min(2)
+        .max(30),
+      about: Joi.string()
+        .required()
+        .min(2)
+        .max(30),
+    }),
 }), updateProfile);
 
 usersRouter.patch('/users/me/avatar', auth, celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().required().uri(),
-  }),
+  body: Joi.object()
+    .keys({
+      avatar: Joi.string()
+        .required()
+        .custom(method, 'custom validation'),
+    }),
 }), updateAvatar);
 
 module.exports = usersRouter;
